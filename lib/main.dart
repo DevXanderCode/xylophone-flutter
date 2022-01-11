e09c8c5706e52ceb1afb9080e5ea37a3adf2e8c8
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-11 03:00:01 +0100

    added the english words package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,9 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Text(adjectives.last),
+          ),
         ),
       ),
     );
